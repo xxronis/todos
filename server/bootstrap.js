dcefeac276540ed8b4cb6a8eadede1dcc5cdf1c3
Panos Chronopoulos <xxronis@gmail.com>
2013-04-29 17:04:58 +0300

    initial commit

---
+++ server/bootstrap.js
@@ -0,0 +1,28 @@
+// if the database is empty on server start, create some sample data.
+Meteor.startup(function () {
+  if (Lists.find().count() === 0) {
+    var data = [
+      {name: "Openbet",
+       contents: [
+       ]
+      },
+      {name: "Getreal",
+       contents: [
+       ]
+      }
+    ];
+
+    var timestamp = (new Date()).getTime();
+    for (var i = 0; i < data.length; i++) {
+      var list_id = Lists.insert({name: data[i].name});
+      for (var j = 0; j < data[i].contents.length; j++) {
+        var info = data[i].contents[j];
+        Todos.insert({list_id: list_id,
+                      text: info[0],
+                      timestamp: timestamp,
+                      tags: info.slice(1)});
+        timestamp += 1; // ensure unique timestamp.
+      }
+    }
+  }
+});
