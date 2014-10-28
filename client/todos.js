fb3ab0bfd95fd76fc660078111f7f1f3f0c9edc8
Pan Chronopoulos <xxronis@gmail.com>
2014-10-29 00:52:36 +0200

    collapsible panels for todos

--- client/todos.js
+++ client/todos.js
@@ -218,6 +218,10 @@ Template.todo_item.adding_tag = function () {
   return Session.equals('editing_addtag', this._id);
 };
 
+Template.todo_item.todo_collapse_id = function () {
+  return this._id;
+};
+
 Template.todo_item.events({
   'click .check': function () {
     Todos.update(this._id, {$set: {done: !this.done}});
